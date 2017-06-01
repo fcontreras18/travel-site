42aabe90a86e54b8af0bbe5fd5783b49870d5d9d
Francisco Echeverria <fcontreras18@gmail.com>
2017-06-01 10:31:31 -0700

    Completed gulp build task

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 8);
+/******/ 	return __webpack_require__(__webpack_require__.s = 9);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -72,7 +72,8 @@
 /* 2 */,
 /* 3 */,
 /* 4 */,
-/* 5 */
+/* 5 */,
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -919,7 +920,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 })(window, document);
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -1618,7 +1619,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
@@ -3169,17 +3170,17 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
 
 
 /***/ }),
-/* 8 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(5);
+__webpack_require__(6);
+
+__webpack_require__(8);
 
 __webpack_require__(7);
-
-__webpack_require__(6);
 
 /***/ })
 /******/ ]);
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,6 +6,7 @@ gulp.task('scripts', ['modernizr'], function(callback) {
     if (err) {
       console.log(err.toString());
     }
+    
     console.log(stats.toString());
     callback();
   });
